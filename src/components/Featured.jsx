8740333ffe1f5e83ad13831b99dd84c74d1f972d
Dash10107 <dakshcjain@gmail.com>
2024-10-09 19:49:59 +0530

    added seperate dynaminc page with carausel for individual destination

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { MapPin } from 'lucide-react'
+import { Link } from 'react-router-dom'
 export default function Featured() {
   return (
     <section className="w-full py-12 md:py-24 lg:py-2 ">
@@ -34,10 +35,18 @@ function FeaturedCard({ image, title, description }) {
       <div className="p-4">
         <h3 className="text-xl font-semibold mb-2">{title}</h3>
         <p className="text-gray-500 ">{description}</p>
-        <div className="mt-4 flex items-center text-sm text-gray-700">
+        {/* properly added readm more to the right corner of the div for better ui */}
+        <div className="mt-4 flex items-center justify-between  text-sm text-gray-700">
+         <div className=' flex items-center justify-between'> 
           <MapPin className="h-4 w-4 mr-1" />
           <span>Popular destination</span>
+          </div>
+                  {/* Link to detailed blog page */}
+        <Link to={`/blog/${title}`} className="text-blue-500 mt-4 inline-block">
+          Read More
+        </Link>
         </div>
+
       </div>
     </div>
   )
