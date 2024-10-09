33505fd7f991debbe8b190d6962be5bbcfee5f2f
Vansh-Parate <vanshparate@gmail.com>
2024-10-09 08:03:55 +0530

    dest-update

---
+++ src/components/CountryGrid.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CountryGrid() {
+  return (
+    <div className="grid md:grid-cols-3 sm:grid-cols-1 gap-6 px-[10rem] py-[4rem]">
+      
+    </div>
+  )
+}
+
+export default CountryGrid;
--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import DestCard from '../components/DestCard';
+import CountryGrid from '../components/CountryGrid';
 
 function Destination() {
     return (
-        <div className="w-screen">
-            Destination
+        <>
+        <div className="container mx-auto px-4 py-6">
+            <h2 className="flex justify-center items-center text-2xl font-serif font-thin ">
+                BROWSE BY POPULAR DESTINATIONS
+            </h2>
+            <DestCard />
+            <h2 className="flex justify-center items-center text-2xl font-serif font-thin ">
+                BROWSE BY COUNTRIES
+            </h2>
+            <CountryGrid />
         </div>
+        </>
     );
 }
 
